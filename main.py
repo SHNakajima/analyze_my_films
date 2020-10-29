7785440bc0e239be5a1cdcbba2cf65da6923f45f
shotaro nakajima <nakajimathirddisp@gmail.com>
2020-10-29 10:06:32 +0900

    add: jupyter notebookで動作するもの

---
+++ main.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+# Seleniumをあらゆる環境で起動させるChromeオプション
+options = Options()
+options.add_argument('--disable-gpu');
+options.add_argument('--disable-extensions');
+options.add_argument('--proxy-server="direct://"');
+options.add_argument('--proxy-bypass-list=*');
+options.add_argument('--start-maximized');
+# options.add_argument('--headless'); # ※ヘッドレスモードを使用する場合、コメントアウトを外す
+
+DRIVER_PATH = '{{ ./chromedriver }}'
+# DRIVER_PATH = '/Users/Kenta/Desktop/Selenium/chromedriver' # ローカル
+# DRIVER_PATH = '/app/.chromedriver/bin/chromedriver'        # heroku
+
+# ブラウザの起動
+driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
